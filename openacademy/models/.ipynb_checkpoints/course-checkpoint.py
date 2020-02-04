21ed04beeaa66b43c168da68736e61f5912311ee
ifixthat <jam@openerp.com>
2020-02-04 19:17:50 +0000

    [ADD] addec course fields

--- openacademy/models/.ipynb_checkpoints/course-checkpoint.py
+++ openacademy/models/.ipynb_checkpoints/course-checkpoint.py
@@ -1,7 +1,13 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models
+from odoo import models, fields
 
 class OpenAcademyCourse(models.Model):
     _name = 'openacademy.course'
     _description =  'OpenAcademy Course'
+    
+    
+    name = fields.Char(string='Course Title', required=True, 
+                        index=True, help='Enter your course title on this field.')
+    description = fields.Html(string='Description')
+    banner = fields.Binary(string='Banner')
