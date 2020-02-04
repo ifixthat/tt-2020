fff7556af11686477beb8fb7e5dad398c82dfbb5
ifixthat <jam@openerp.com>
2020-02-04 21:21:00 +0000

    [ADD] d2 afternoon

--- openacademy/models/course.py
+++ openacademy/models/course.py
@@ -2,6 +2,14 @@
 
 from odoo import models, fields
 
+
+class OpenAcademyTags(models.Model):
+    _name = 'openacademy.tags'
+    _description ='OpenAcademy Tags'
+
+    name = fields.Char(string='Name')
+
+    
 class OpenAcademyCourse(models.Model):
     _name = 'openacademy.course'
     _description =  'OpenAcademy Course'
@@ -12,3 +20,10 @@ class OpenAcademyCourse(models.Model):
     description = fields.Html(string='Description')
     banner = fields.Binary(string='Banner')
     price = fields.Float(string='Price', digits=(5, 4))
+    expire_date = fields.Date(string='Expire After', required=True)
+    
+    responsible_id = fields.Many2one(comodel_name='res.users', required=True,
+                        string='Responsible', ondelete='restrict', copy=False)
+    
+    tag_ids = fields.Many2many(comodel_name='openacademy.tags', relation='rel_course_tags',
+                            column1='course_id', column2='tag_id', string='Tags')
