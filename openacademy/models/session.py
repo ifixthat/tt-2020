04a682463214e8ce275a829ea6823e578b4a2048
ifixthat <jam@openerp.com>
2020-02-05 19:40:14 +0000

    [ADD] D2 noon code

--- openacademy/models/session.py
+++ openacademy/models/session.py
@@ -6,17 +6,26 @@ from odoo import models, fields
 class OpenAcademySession(models.Model):
     _name = 'openacademy.session'
     _description =  'OpenAcademy Sessions'
-    _order = 'id, expire_date desc'
+    _order = 'id, start_date'
     
     name = fields.Char(string='Name', required=True, 
                         index=True, help='Enter your course title on this field.')
     active = fields.Boolean(string='Active', default=True)
-    avail_seat_per = fields.Float(string='Avaliable Seats (%)')
-    max_seats = fields.Integer(string='Maximum Seats')
-    
-    
+    course_id = fields.Many2one(comodel_name='openacademy.course', required=True)
     insructor_id = fields.Many2one(comodel_name='res.partner', required=True,
                         string='Instructor', ondelete='restrict', copy=False)
-    
-    tag_ids = fields.Many2many(comodel_name='openacademy.tags', relation='rel_course_tags',
-                            column1='course_id', column2='tag_id', string='Tags')
+    location_id = fields.Many2one(comodel_name='res.partner', ondelete='restrict')
+    code = fields.Char(string='Code', size=32)
+    start_date = fields.Datetime(string='Start Date', required=True)
+    end_date = fields.Datetime(string='End Date', required=True)
+    avail_seat_per = fields.Float(string='Avaliable Seats (%)')
+    max_seats = fields.Integer(string='Maximum Seats')
+    min_seats = fields.Integer(string='Minimum Required Seats')
+    syllabus_notes = fields.Html(string='Syllabus')
+    state = fields.Selection(selection=[
+                            ('draft', 'New'),
+                            ('approve', 'Approved'),
+                            ('confirm', 'Confirmed'),
+                            ('cancel', 'Cancelled'),
+                            ('done', 'Done'),
+                        ])
