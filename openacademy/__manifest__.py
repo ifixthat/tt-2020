8ba100969671a2e68bef2e44ce485a24d4a382e6
Jigar A <jam@openerp.com>
2020-02-06 23:42:48 +0000

    [ADD] d4 eod

--- openacademy/__manifest__.py
+++ openacademy/__manifest__.py
@@ -16,7 +16,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['contacts'],
+    'depends': ['contacts', 'web_map'],
 
     # always loaded
     'data': [
